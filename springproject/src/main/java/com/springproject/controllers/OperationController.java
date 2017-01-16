603247ebea4be5ecff220e040ac63fee1aa3705e
javiladron <javi_ilegales@hotmail.com>
2017-01-16 21:25:51 +0100

    init operation controller

--- springproject/src/main/java/com/springproject/controllers/OperationController.java
+++ springproject/src/main/java/com/springproject/controllers/OperationController.java
@@ -1,5 +1,16 @@
 package com.springproject.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class OperationController {
 
+	@RequestMapping(value="/operation/",method = RequestMethod.GET)
+	public String showDefaultIndex(Model model){
+		return "index";
+	}
+	
 }
